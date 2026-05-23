6dba501e24ce1fae0b7125f47eb8c2d412fe9a9d
Stuart Donaldson <stuart.donaldson@gmail.com>
2026-05-22 21:30:53 -0700

    feat(webapp): doGet self-registers WEBAPP_URL via ScriptApp.getService().getUrl()

--- src/WebApp.js
+++ src/WebApp.js
@@ -1,3 +1,9 @@
+function doGet(e) {
+  var url = ScriptApp.getService().getUrl();
+  PropertiesService.getScriptProperties().setProperty('WEBAPP_URL', url);
+  return ContentService.createTextOutput('WEBAPP_URL registered: ' + url);
+}
+
 function doPost(e) {
   var payload = JSON.parse(e.postData.contents);
   var expected = PropertiesService.getScriptProperties().getProperty('WEBAPP_SECRET');
